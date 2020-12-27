0971265970518493c3c4cf5356e7e9cc751235da
amirabbas-aslami <abas.aslmi0007@gmail.com>
2020-12-27 12:01:42 +0330

    videoplayer

---
+++ lib/pages/video_player.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class VideoPlayerPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() =>VideoPlayerPageState();
+
+}
+
+class VideoPlayerPageState  extends State<VideoPlayerPage>{
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
